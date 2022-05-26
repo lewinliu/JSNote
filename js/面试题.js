4d6fc69632e67230be6bab91a7782a815a427dbf
LLW <10932017+liwen-liu@user.noreply.gitee.com>
2022-05-26 17:25:02 +0800

    Number()及parseInt()的类型转换关系

--- js/面试题.js
+++ js/面试题.js
@@ -5,7 +5,7 @@ console.log(a); */
 *
 10+null->10+0->10
 10+true->10+1->11
-11 +[]-> 11+''-> '11' 空数组变为数字，先要经历变为空字符串，遇到字符串，啥都别想了，直接变为字符串拼接
+11 +[]-> 11+''-> '11' 空数组变为数字,先要经历变为空字符串,遇到字符串,啥都别想了,直接变为字符串拼接
 
 '11undefined明明null10false'
 */
@@ -79,4 +79,98 @@ console.log(i);
  */
 
 
+/* // 2.分别写出下列表达式输出的结果(腾讯面试题)
+var a = 'abc' + 123 + 456;  // 'abc123456';
+var b = '456' - '123';  // 333;
+var c = 100 + true + 21.2 + null + undefined + "Tencent" + [] + null + 9 + false;
+// c = (100 + 1 + 21.2 + 0 + NaN) + "Tencent" + "" + "null" + "9" + "false";
+// c = NaN + "Tencent" + "" + "null" + "9" + "false";
+// C = "NaNTencentnull9false";
+console.log(a, b, c); 
+*/
 
+/* // 3.写出下列代码的输出结果(腾讯面试题)
+var str = 'abc123';
+var num = parseInt(str);    // num=NaN;
+if (num == NaN) {
+    alert(NaN); 
+} else if (num == 123) {
+    alert(123);
+} else if (typeof num == ' number') {   // true;
+    alert('number');   // 输出结果；
+} else {
+    alert('str');
+} */
+
+/* // 4.写出下列代码的输出结果( 腾讯面试题)
+var a = 0;
+var b = a;
+
+b++;
+alert(a)// 0;
+
+var o = {};
+o.a = 0;
+
+var b = o;
+b.a = 10;
+alert(o.a)// 10;
+ */
+
+// // 5.能输出”1”的有哪些?  A,字符串‘1’
+// A. alert(1)
+// B. console. Log(parseInt(1.3) )
+// C. console. log(1)
+// D. console. log(isNaN(1))
+// E. console. log(parseInt("1"))
+
+
+// // 6.下面结果是"undefined"的是：B
+// A. console. log(alert(1))
+// B. typeof undefined
+// C. console. log (par seInt (undefined) )
+// D. isNaN(undefined)
+
+/* //7.下面结果能得到true的是：
+A. isNaN(null)
+B. isNaN(parseInt(null))
+C. Number(null)
+D. parseFloat(null) */
+
+// // 8.输出下面程序的结果
+// parseInt("") // NaN
+// Number("")	// 0
+// isNaN("")    // false
+// parseInt(null)   // NaN
+// Number(null) // 0
+// isNaN(null)  // false
+// parseInt("12px") // 12
+// Number("12px")   // NaN,Number()中出现的字符串出现任意一个非有效字符,结果都是NaN
+// isNaN("12px")    // true,通过Number()转化成数字,结果为NaN
+
+// // 9.下面程序输出的结果：培训
+//  // isNaN(NaN) -> true
+//  // "" -> 0 -> false
+// if (isNaN(NaN) == "") {  
+//     console.log("珠峰")
+// } else {
+//     console.log("培训")
+// }
+
+// 10.下面程序输出的结果
+let x = [1, 2, 3];
+let y = x;
+let Z = [4, 5, 6];
+// [1, 2, 3]; [4, 5, 6];
+y[0] = 10;
+// [10, 2, 3]; [4, 5, 6];
+y = z;
+z[1] = 20;
+// [10, 2, 3]; [4, 20, 6];
+x[2] = z = 30;
+// ?
+console.Log(x, y, z);
+
+
+
+//
