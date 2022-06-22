7408cf7d8c678f72ebe36d6e8cbf799e7f8cba7c
lewinliu <747108698@qq.com>
2022-06-22 17:00:06 +0800

    JSON字符串转换对象

--- js/商城排序案例-bootstrap.js
+++ js/商城排序案例-bootstrap.js
@@ -22,18 +22,39 @@ Ul组件库
     let DATA = null,
         xhr = new XMLHttpRequest;
     // 打开一个链接请求，基于GET请求和同步编程完成
-    let url = 'data/data.json';
-    xhr.open('GET', url, false);
+    let filePath = 'josn/data.json';
+    xhr.open('GET', filePath, false);
     // 监听服务器返回的状态信息（在HTTP状态码为200，请求状态为4的时候能拿到数据）
     xhr.onreadystatechange = function () {
         if (xhr.status === 200 && xhr.readyState === 4) {
-            // 基于responseText获取响应回来的信息
+            // 基于responseText获取响应回来的信息(JSON格式字符串)
             DATA = xhr.responseText;
         }
     };
     // 发送AJAX请求
     xhr.send();
+    //把获取的JSON字符串转换为对象
+    DATA = JSON.parse(DATA);
+
     console.log(DATA);
 }();
 
 
+
+/* 
+// JSON字符串转换为对象
+let str = '{"name":"靓仔"}';
+let obj = JSON.parse(str);
+
+// => 在IE低版本浏览器中，全局对象 window 中没有JSON这个对象，也就没有所谓的 parse 等方法了
+obj = eval('(' + str + ')');
+// => 基于 eval 可以实现，但是为了保证转换格式正确，我们需要把字符串用小括号包起来转换 
+*/
+
+
+// 把对象转换为JSON格式字符串: JSON.stringify
+let obj = {
+    name: "靓仔"
+};
+let str = JSON.stringify(obj); // 字符串化 
+console.log(str);
