5fc9cdee4bb15a8cff3512784d30f942798f72fb
LLW <10932017+liwen-liu@user.noreply.gitee.com>
2022-06-22 09:40:09 +0800

    商城排序

--- js/商城排序案例-bootstrap.js
+++ js/商城排序案例-bootstrap.js
@@ -12,3 +12,28 @@ Ul组件库
     react : antd
     EasyUl / AmazeUl ...
 */
+
+~ function () {
+    /*
+        第一步:从服务器获取需要展示的数据，然后绑定在页面中
+    */
+    // 1.基于AJAX获取服务器端数据,把数据存储到DATA中
+    // 创建AJAX的实例
+    let DATA = null,
+        xhr = new XMLHttpRequest;
+    // 打开一个链接请求，基于GET请求和同步编程完成
+    let url = 'data/data.json';
+    xhr.open('GET', url, false);
+    // 监听服务器返回的状态信息（在HTTP状态码为200，请求状态为4的时候能拿到数据）
+    xhr.onreadystatechange = function () {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            // 基于responseText获取响应回来的信息
+            DATA = xhr.responseText;
+        }
+    };
+    // 发送AJAX请求
+    xhr.send();
+    console.log(DATA);
+}();
+
+
